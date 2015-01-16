1a534f296aa1d085aaf2e3bda5f765510f565415
尹现伟 <yinxianwei@qq.com>
2015-01-16 10:44:55 +0800

    Initial Commit

---
+++ GetAllFuncList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GetAllFuncList
+//
+//  Created by 尹现伟 on 15-1-16.
+//  Copyright (c) 2015年 DNE Technology Co.,Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ GetAllFuncList/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  GetAllFuncList
+//
+//  Created by 尹现伟 on 15-1-16.
+//  Copyright (c) 2015年 DNE Technology Co.,Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
